aa3bc5f672a173936ee3f50d88d85367a311a69b
James Forbes <james.a.forbes@gmail.com>
2021-09-09 16:15:18 +1000

    Improve build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,56 @@
-
 import {terser} from 'rollup-plugin-terser'
 
-export default {
-    input: './lib/index.js'
-    ,plugins: [
-        terser()
-    ]
-    ,output: {
-        file: './dist/superouter.min.js'
-        ,format: 'umd'
-        ,name: 'superouter'
-        ,sourcemap: 'external'
-    },
 
-}
+export default [
+    {
+        input: './lib/index.js'
+        ,output: {
+            file: './dist/superouter.umd.js'
+            ,format: 'umd'
+            ,name: 'superouter'
+            ,sourcemap: 'external'
+        },
+    
+    }
+    ,{
+        input: './lib/index.js'
+        ,output: {
+            file: './dist/superouter.cjs'
+            ,format: 'cjs'
+            ,name: 'superouter'
+            ,sourcemap: 'external'
+        },
+    
+    }
+    ,{
+        input: './lib/index.js'
+        ,plugins: [
+            terser()
+        ]
+        ,output: {
+            file: './dist/superouter.umd.min.js'
+            ,format: 'umd'
+            ,name: 'superouter'
+            ,sourcemap: 'external'
+        },
+    }
+    ,{
+        input: './lib/index.js'
+        ,output: {
+            file: './dist/superouter.esm.js'
+            ,format: 'esm'
+            ,sourcemap: 'external'
+        },
+    }
+    ,{
+        input: './lib/index.js'
+        ,plugins: [
+            terser()
+        ]
+        ,output: {
+            file: './dist/superouter.esm.min.js'
+            ,format: 'esm'
+            ,sourcemap: 'external'
+        },
+    }
+]
